33ae08bee377ac2e2d6900403d62266ba4de378d
DaShPaRkEr <62864632+DaShPaRkEr@users.noreply.github.com>
2022-02-13 00:33:32 -0500

    Add files via upload

---
+++ docs/release/1.0/update.cmd
@@ -0,0 +1,39 @@
+@echo off
+color 3
+title Sana Client Updater
+echo Hello, we're about to update Sana Client for you! (:
+echo This may take a few minutes depending on your wifi speed, and hard drive speed...
+echo preparing...
+timeout /t 2 /nobreak
+echo.
+
+cd..\..\
+echo Downloading updates...
+echo.
+curl -o bin.zip sanaclient.xyz/release/1.0/bin.zip
+curl -o sc.new sanaclient.xyz/release/1.0/sc.new
+echo Deleting Sana Client Launcher...
+del /f /s /q ".\Sana Client.exe"
+echo.
+echo Extracting new files...
+tar -xf bin\resources\bin.zip -C bin\
+echo done
+echo.
+echo Copying new launcher...
+xcopy bin\resources\sc.new
+echo.
+echo Renaming launcher...
+ren sc.new "Sana Client.exe"
+echo.
+echo **************
+echo      DONE
+echo **************
+pause
+echo.
+echo Sana Client ver 1.0 has been loaded onto your system...
+echo feel free to use Sana Client!
+echo SanaClient.xyz/release/1.0
+echo.
+pause
+start "sana client.exe"
+exit
